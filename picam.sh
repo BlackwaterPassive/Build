0ebab42609be682e5c797e83bda6f30a0bb0fdd0
Home Builder <kwitkosski@me.com>
2017-06-18 11:27:16 -0700

    Build pic.

--- picam.sh
+++ picam.sh
@@ -3,6 +3,6 @@
 DATE=$(date +"%Y-%m-%d_%H%M")
 
 raspistill -n -vf -hf -o /home/pi/Build/$DATE.jpg
-git add /home/pi/Build/$DATE.jpg
+git add $DATE.jpg
 git commit -a -m 'Build pic.'
-git push /home/pi/Build/$DATE.jpg
+git push $DATE.jpg
