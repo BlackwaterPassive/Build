45f93a7ce5797a0c10b3848476e3dcb53a394d80
Darren Ditto <darren.ditto@alumni.unbc.ca>
2017-06-24 15:33:04 -0700

    added pic set

--- picam.sh
+++ picam.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/usr/bin/raspistill -n -vf -hf -o /home/pi/Build/$(date +"%Y_%m_%d_%H%M").jpg
+DATE=$(date +"%Y_%m_%d_%H%M") 
+
+/usr/bin/raspistill -n -vf -hf -o /home/pi/Build/$DATE.jpg 
