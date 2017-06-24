61da2a27bec25383624dca22b236f7abfe729e03
Darren Ditto <darren.ditto@alumni.unbc.ca>
2017-06-23 22:46:03 -0700

    added pic set

--- picam.sh
+++ picam.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-DATE=$(date +"%Y_%m_%d_%H%M")
-
-/usr/bin/raspistill -n -vf -hf -o /home/pi/Build/$DATE.jpg
-
-cd /home/pi/Build/
-git add /home/pi/Build/$DATE.jpg
-git commit -a -m 'added pic'
-git push origin
+/usr/bin/raspistill -n -vf -hf -o /home/pi/Build/$(date +"%Y_%m_%d_%H%M").jpg
