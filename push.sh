c645fa906b7478aacd2123324ccf0b863e6c942a
Darren Ditto <darren.ditto@alumni.unbc.ca>
2017-06-24 17:31:46 -0700

    commit message, to avoid being prompted interactively

--- push.sh
+++ push.sh
@@ -1,6 +1,26 @@
-#!/bin/bash
+#!/bin/sh
 
-cd /home/pi/
-/usr/bin/git -C /home/pi/Build/ add .
-/usr/bin/git -C /home/pi/Build/ commit -m "added pic set"
-/usr/bin/git -C /home/pi/Build/ push origin master
+cd /home/pi/Build/
+
+git add *
+if [[ $? != 0 ]] 
+then 
+   mail -s "add failed" darren.ditto@alumni.unbc.ca
+   exit 1
+fi
+
+git commit -a -m "commit message, to avoid being prompted interactively"
+if [[ $? != 0 ]] 
+then 
+   mail -s "commit failed" darren.ditto@alumni.unbc.ca
+   exit 1
+fi
+
+git push
+if [[ $? != 0 ]] 
+then 
+   mail -s "push failed" darren.ditto@alumni.unbc.ca
+   exit 1
+fi
+
+mail -s "push ok" darren.ditto@alumni.unbc.ca
